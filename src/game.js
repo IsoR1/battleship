1220be65de7f1aa509dca6388f3f88901358eb7c
Braco <isovicramiz07@gmail.com>
2023-05-30 03:43:15 -0400

    Started refactoring game.js

--- src/game.js
+++ src/game.js
@@ -1,72 +1,278 @@
 const createShip = require('./ship');
 const createGameBoard = require('./gameboard');
 const { createPlayer, createAi } = require('./player');
-const { updateAttackResult } = require('./mainContent');
-const { isGameOver } = require('./gameutils');
+const { endGame } = require('./gameutils');
+const {
+  renderHeader, createForm, createAttackInstructions, createAttackResultsContainer, updateAttackResult, createMainContent,
+} = require('./mainContent');
 
-// const gameLoop = async (p1, ai, attackInput, gameState) => {
-//   if (!gameState.isGameOver) {
-//     if (gameState.currentPlayer === p1) {
-//       if (p1.gameBoard.allShipsSunk()) {
-//         gameState.isGameOver = true;
-//         console.log(`${p1.name} Wins!`);
-//         return `${p1.name} Wins!`;
-//       }
-//       const result = p1.attackEnemyGameBoard(attackInput);
-//       console.log(result);
-//       updateAttackResult(p1.name, result);
-//       gameState.currentPlayer = ai;
-//     }
-//     if (gameState.currentPlayer === ai) {
-//       if (ai.gameBoard.allShipsSunk()) {
-//         gameState.isGameOver = true;
-//         console.log('Ai Wins!');
-//         return 'Ai Wins!';
-//       }
-//       await new Promise((resolve) => setTimeout(resolve, 2000));
+// // const gameLoop = async (p1, ai, attackInput, gameState) => {
+// //   if (!gameState.isGameOver) {
+// //     if (gameState.currentPlayer === p1) {
+// //       if (p1.gameBoard.allShipsSunk()) {
+// //         gameState.isGameOver = true;
+// //         console.log(`${p1.name} Wins!`);
+// //         return `${p1.name} Wins!`;
+// //       }
+// //       const result = p1.attackEnemyGameBoard(attackInput);
+// //       console.log(result);
+// //       updateAttackResult(p1.name, result);
+// //       gameState.currentPlayer = ai;
+// //     }
+// //     if (gameState.currentPlayer === ai) {
+// //       if (ai.gameBoard.allShipsSunk()) {
+// //         gameState.isGameOver = true;
+// //         console.log('Ai Wins!');
+// //         return 'Ai Wins!';
+// //       }
+// //       await new Promise((resolve) => setTimeout(resolve, 2000));
 
-//       const result = ai.attackEnemyGameBoard();
-//       console.log('Ai turn:', result);
-//       updateAttackResult('ai', result);
-//       gameState.currentPlayer = p1;
-//     }
+// //       const result = ai.attackEnemyGameBoard();
+// //       console.log('Ai turn:', result);
+// //       updateAttackResult('ai', result);
+// //       gameState.currentPlayer = p1;
+// //     }
+// //   }
+// // };
+// const gameState = {
+//   isGameOver: false,
+//   currentPlayer: p1,
+//   hasGameStarted: false,
+//   isAiTurn: false,
+// };
+
+// const checkWinner = (p1, ai, gameState) => {
+//   if (p1.gameBoard.allShipsSunk() && ai.gameBoard.allShipsSunk()) {
+//     endGame(0, gameState);
+//   }
+
+//   if (p1.gameBoard.allShipsSunk()) {
+//     const winner = endGame(p1.name, gameState);
+//     console.log(winner);
+//     return;
+//   }
+
+//   if (ai.gameBoard.allShipsSunk()) {
+//     endGame(ai, gameState);
 //   }
 // };
 
-const gameLoop = async (p1, ai, attackInput, gameState) => {
-  if (gameState.isGameOver) {
-    return; // Game is already over, exit the loop.
-  }
+// const gameLoop = async (p1, ai, attackInput, gameState) => {
+//   // if (gameOver(p1, ai)) {
+//   //   gameState.isGameOver = true;
+//   //   // console.log('Ai Wins!');
+//   //   // return 'Ai Wins!';
+//   // }
+//   // if (p1.gameBoard.allShipsSunk() && ai.gameBoard.allShipsSunk()) {
+//   //   endGame(0, gameState);
+//   // }
 
-  if (gameState.currentPlayer === p1) {
-    if (isGameOver(p1, ai)) {
-      gameState.isGameOver = true;
-      console.log(`${p1.name} Wins!`);
-      return `${p1.name} Wins!`;
-    }
+//   // if (p1.gameBoard.allShipsSunk()) {
+//   //   const winner = endGame(p1.name, gameState);
+//   //   console.log(winner);
+//   //   return;
+//   // }
 
-    const result = p1.attackEnemyGameBoard(attackInput);
-    console.log(result);
-    updateAttackResult(p1.name, result);
-    gameState.isAITurn = true; // Set the flag for AI's turn
-    gameState.currentPlayer = ai;
-  }
+//   // if (ai.gameBoard.allShipsSunk()) {
+//   //   endGame(ai, gameState);
+//   // }
 
-  if (gameState.currentPlayer === ai && gameState.isAITurn) {
-    if (isGameOver(p1, ai)) {
-      gameState.isGameOver = true;
-      console.log('Ai Wins!');
-      return 'Ai Wins!';
-    }
+//   const result = p1.attackEnemyGameBoard(attackInput);
+//   console.log(result);
+//   // updateAttackResult(p1.name, result);
+//   gameState.isAITurn = true; // Set the flag for AI's turn
+//   gameState.currentPlayer = ai;
+//   // }
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+//   if (gameState.currentPlayer === ai && gameState.isAITurn) {
+//     // if (isGameOver(p1, ai)) {
+//     //   gameState.isGameOver = true;
+//     //   console.log('Ai Wins!');
+//     //   return 'Ai Wins!';
+//     // }
 
-    const result = ai.attackEnemyGameBoard();
-    console.log('Ai turn:', result);
-    updateAttackResult('ai', result);
-    gameState.isAITurn = false; // Reset the flag for AI's turn
-    gameState.currentPlayer = p1;
-  }
+//     await new Promise((resolve) => setTimeout(resolve, 1500));
+
+//     const result = ai.attackEnemyGameBoard();
+//     console.log('Ai turn:', result);
+//     // updateAttackResult('ai', result);
+//     gameState.isAITurn = false; // Reset the flag for AI's turn
+//     gameState.currentPlayer = p1;
+
+//     // checkWinner(p1, ai, gameState);
+//   }
+// };
+
+// module.exports = { checkWinner, gameLoop };
+
+const createGame = () => {
+  const game = {
+    p1: null,
+    ai: null,
+    gameState: {
+      isGameOver: false,
+      currentPlayer: null,
+      hasGameStarted: false,
+      isAITurn: false,
+    },
+    handleNameSubmit() {
+      const p1GameBoard = createGameBoard();
+      const aiGameBoard = createGameBoard();
+      const nameInput = document.getElementById('player-name');
+      const nameSubmitButton = document.querySelector('.name-button');
+      const nameFormDiv = document.querySelector('.name-input-div');
+      nameSubmitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (nameInput.value) {
+          this.p1 = createPlayer(nameInput.value, aiGameBoard);
+          this.ai = createPlayer(p1GameBoard);
+          p1GameBoard.createGrid();
+          aiGameBoard.createGrid();
+          nameFormDiv.style.display = 'none';
+          this.startGame();
+          // createMainContent(p1.gameBoard);
+        }
+      });
+    },
+    startGame() {
+      // const p1GameBoard = createGameBoard();
+      // const aiGameBoard = createGameBoard();
+      // this.p1 = createPlayer(playerName, aiGameBoard);
+      // this.ai = createPlayer(p1GameBoard);
+      // p1GameBoard.createGrid();
+      // aiGameBoard.createGrid();
+      this.gameState.currentPlayer = this.p1;
+      this.gameState.hasGameStarted = true;
+      createMainContent(this.p1.gameBoard);
+    },
+
+    placeShipsFromPlayerInput() {
+      const selectedAlignment = document.querySelector('input[name="alignment"]:checked').value;
+
+      const ships = [
+        { name: 'carrier', length: 5, inputId: 'carrier' },
+        { name: 'battleship', length: 4, inputId: 'battleship' },
+        { name: 'cruiser', length: 3, inputId: 'cruiser' },
+        { name: 'submarine', length: 3, inputId: 'submarine' },
+        { name: 'destroyer', length: 2, inputId: 'destroyer' },
+      ];
+      const numShips = ships.length;
+
+      const numShipsPlaced = 0;
+
+      ships.forEach((ship) => {
+        const input = document.getElementById(ship.inputId);
+        if (input.value) {
+          const newShip = createShip(ship.name, ship.length);
+          const coord = input.value.split(', ').map(Number);
+          const placement = this.ai.gameBoard.placeShip(coord, newShip, selectedAlignment);
+          if (placement) {
+            input.value = '';
+            input.classList.add('hidden');
+            this.numShipsPlaced += 1;
+          }
+        }
+      });
+      if (numShipsPlaced === numShips) {
+        this.placeAiShips();
+      }
+    },
+    placeAiShips() {
+      // AI ship placement
+      const aiShips = [
+        // { name: 'carrier', length: 5 },
+        // { name: 'battleship', length: 4 },
+        // { name: 'cruiser', length: 3 },
+        // { name: 'submarine', length: 3 },
+        { name: 'destroyer', length: 2 },
+      ];
+
+      aiShips.forEach((ship) => {
+        let isValidPlacement = false;
+        let placementCoord;
+        let placementAlignment;
+
+        while (!isValidPlacement) {
+          const coordRow = Math.floor(Math.random() * this.p1.gameBoard.grid.length);
+          const subArr = this.p1.gameBoard.grid[coordRow];
+          const coordCol = Math.floor(Math.random() * subArr.length);
+          const coord = [coordRow, coordCol];
+          const options = ['VERTICAL', 'HORIZONTAL'];
+          const randomAlignment = options[Math.floor(Math.random() * options.length)];
+          console.log(randomAlignment);
+          console.log(coord);
+
+          const newShip = createShip(ship.name, ship.length);
+          const p1Placement = this.p1.gameBoard.placeShip(coord, newShip, randomAlignment);
+
+          if (p1Placement) {
+            isValidPlacement = true;
+            placementCoord = coord;
+            placementAlignment = randomAlignment;
+          }
+        }
+        console.log('success:', placementAlignment, placementCoord);
+      });
+      console.log('ships on board on start', p1.gameBoard.shipsOnBoard);
+      this.gameState.hasGameStarted = true;
+      this.setupGameUi();
+    },
+    setupGameUi() {
+      const attackInstructions = createAttackInstructions();
+      const hitMissDivCon = createAttackResultsContainer();
+      const underHeaderDiv = document.querySelector('.main-body-div');
+      const gameBoardDiv = document.querySelector('.game-board-div');
+      const shipPlacementContainer = document.querySelector('.ship-placement-container');
+      shipPlacementContainer.remove();
+      underHeaderDiv.insertBefore(attackInstructions, underHeaderDiv.firstChild);
+      underHeaderDiv.insertBefore(hitMissDivCon, underHeaderDiv.children[1]);
+      underHeaderDiv.style.flexDirection = 'column';
+      gameBoardDiv.style.justifyContent = 'center';
+      gameBoardDiv.style.alignItems = 'center';
+    },
+    endGame(winner) {
+      this.gameState.isGameOver = true;
+
+      if (winner === 0) {
+        return 'Tie';
+      }
+      return `Player ${winner} wins!`;
+    },
+    aiAttack() {
+      const result = this.ai.attackEnemyGameBoard();
+      console.log('Ai turn:', result);
+      updateAttackResult('ai', result);
+      this.gameState.isAITurn = false; // Reset the flag for AI's turn
+      this.gameState.currentPlayer = this.p1;
+    },
+    playerAttack(attackInput) {
+      const result = this.p1.attackEnemyGameBoard(attackInput);
+      console.log(result);
+      // updateAttackResult(p1.name, result);
+      this.gameState.isAITurn = true; // Set the flag for AI's turn
+      this.gameState.currentPlayer = this.ai;
+    },
+    attackLoop() {
+      if (!this.gameState.isGameOver) {
+        const clickHandler = (e) => {
+          if (e.target.matches('.col-div') && gameState.hasGameStarted && gameState.currentPlayer === p1) {
+            const input = [e.target.dataset.rowId, e.target.dataset.colId];
+            console.log(input);
+            const result = console.log('ships on board', this.p1.gameBoard.shipsOnBoard);
+            console.log('ai.gb ships on board', this.ai.gameBoard.shipsOnBoard);
+            console.log(gameState);
+
+            if (gameState.isGameOver) {
+              document.removeEventListener('click', clickHandler);
+            }
+          }
+        };
+
+        document.addEventListener('click', clickHandler);
+      }
+    },
+  };
+  return game;
 };
 
-module.exports = gameLoop;
+module.exports = createGame;
